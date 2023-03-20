10fa29ffe8c08e52264f467e75a64186651c54d3
Dan <danielhymn@gmail.com>
2023-03-20 21:50:02 +1100

    load more

--- src/pages/collection/Tokenlist.tsx
+++ src/pages/collection/Tokenlist.tsx
@@ -12,14 +12,27 @@ export const Tokenlist: React.FC<Props> = ({ collectionId }) => {
   const [tokens, setTokens] = useState<Token[]>([]);
   const [isLoading, setIsLoading] = useState(false);
 
+  const [currentPage, setCurrentPage] = useState(0);
+  const [hasMoreData, setHasMoreData] = useState(true);
+
   useEffect(() => {
     // fetch the first page of tokens
     setIsLoading(true);
-    mockApi.getTokensByCollection(collectionId).then((tokenList) => {
+    mockApi.getTokensByCollection(collectionId, currentPage).then((nextPageData) => {
+      const newTokens = nextPageData.data;
+      const allTokens = [...tokens, ...newTokens];
       setIsLoading(false);
-      setTokens(tokenList.data);
+      setTokens(allTokens);
+
+      if (allTokens.length === nextPageData.pagination.totalRecords) {
+        setHasMoreData(false);
+      }
     });
-  }, []);
+  }, [currentPage]);
+
+  const handleLoadMore = () => {
+    setCurrentPage((currentPage) => currentPage + 1);
+  };
 
   return (
     <>
@@ -29,6 +42,22 @@ export const Tokenlist: React.FC<Props> = ({ collectionId }) => {
           {tokens.map((token) => (
             <TokenCard token={token} key={token.id} />
           ))}
+        </div>
+
+        {/* load more */}
+        <div className="mt-5 px-[20%] lg:px-[30%]">
+          {hasMoreData && (
+            <button
+              disabled={isLoading}
+              onClick={handleLoadMore}
+              className="mt-5 w-full rounded-md bg-gray-800 py-3 text-center text-lg font-medium text-white hover:bg-gray-700"
+            >
+              Load more
+            </button>
+          )}
+          {!hasMoreData && tokens.length !== 0 && (
+            <div className="mt-10 text-center text-lg text-gray-500">{`---- Showing all ${tokens.length} items ---`}</div>
+          )}
         </div>
       </div>
     </>
